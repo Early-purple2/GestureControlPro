106f072cf2bff809f47b10cdd82223057e9560c5
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:46:59 +0200

    Update MLPredictionService.swift

--- Sources/GestureControlPro/Core/Services/MLPredictionService.swift
+++ Sources/GestureControlPro/Core/Services/MLPredictionService.swift
@@ -1,1 +1,8 @@
+import Foundation
+import CoreML
+
+class MLPredictionService {
+    func loadModels() async throws { /*...*/ }
+    func predictGesture(landmarks: [HandLandmark]) async throws -> MLPredictionResult { /*...*/ }
+}
 
