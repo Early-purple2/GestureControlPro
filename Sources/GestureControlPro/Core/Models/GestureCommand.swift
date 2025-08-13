d0a6a4c184a91e3094580c83832c77319a5cfd28
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:43:50 +0200

    Update GestureCommand.swift

--- Sources/GestureControlPro/Core/Models/GestureCommand.swift
+++ Sources/GestureControlPro/Core/Models/GestureCommand.swift
@@ -1,1 +1,11 @@
+import Foundation
+import CoreGraphics
+
+struct GestureCommand: Codable {
+    let id: UUID
+    let action: GestureAction
+    let position: CGPoint
+    let timestamp: TimeInterval
+    let meta [String:String]
+}
 
