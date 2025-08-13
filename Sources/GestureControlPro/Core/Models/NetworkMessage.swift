2e6fd5627e72a8c7ebe8e33144a4167928e4f75e
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:44:31 +0200

    Update NetworkMessage.swift

--- Sources/GestureControlPro/Core/Models/NetworkMessage.swift
+++ Sources/GestureControlPro/Core/Models/NetworkMessage.swift
@@ -1,1 +1,10 @@
+import Foundation
+
+struct NetworkMessage: Codable {
+    let id: UUID
+    let type: MessageType
+    let payload: Data
+    let timestamp: TimeInterval
+    let priority: MessagePriority
+}
 
