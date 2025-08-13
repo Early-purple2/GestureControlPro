61f41667fa1ed523d34bf44d579200a8de5d1a46
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:43:07 +0200

    Update HandLandmark.swift

--- Sources/GestureControlPro/Core/Models/HandLandmark.swift
+++ Sources/GestureControlPro/Core/Models/HandLandmark.swift
@@ -1,1 +1,13 @@
+import Foundation
+import Vision
+import CoreGraphics
+
+struct HandLandmark: Identifiable, Codable {
+    let id: UUID
+    let jointName: VNHumanHandPoseObservation.JointName
+    let position: CGPoint
+    let confidence: Float
+    let timestamp: TimeInterval
+    let handedness: Handedness
+}
 
