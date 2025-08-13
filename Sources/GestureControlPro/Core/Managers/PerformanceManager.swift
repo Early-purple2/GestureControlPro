f258d67a2fcb7fa4beaf53bad09be9c0e2dfba0a
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:48:59 +0200

    Update PerformanceManager.swift

--- Sources/GestureControlPro/Core/Managers/PerformanceManager.swift
+++ Sources/GestureControlPro/Core/Managers/PerformanceManager.swift
@@ -1,1 +1,11 @@
+import Foundation
+
+class PerformanceManager: ObservableObject {
+    static let shared = PerformanceManager()
+    @Published var currentFPS: Double = 0
+    @Published var networkLatency: TimeInterval = 0
+    func startMonitoring() async { /*...*/ }
+    func updateGestureDetectionMetrics(fps: Double, processingTime: TimeInterval) { /*...*/ }
+    func updateNetworkMetrics(latency: TimeInterval, bytesSent: Int, bytesReceived: Int) { /*...*/ }
+}
 
