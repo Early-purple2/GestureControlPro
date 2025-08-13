b6ad2d612c3319761410dcf039c39c3d0acf045f
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:46:28 +0200

    Update MetalService.swift

--- Sources/GestureControlPro/Core/Services/MetalService.swift
+++ Sources/GestureControlPro/Core/Services/MetalService.swift
@@ -1,1 +1,10 @@
+import Metal
+import MetalPerformanceShaders
+
+class MetalService {
+    static let shared = MetalService()
+    private init() {}
+    func configure(with device: MTLDevice) { /*...*/ }
+    func setupHandTrackingPipeline() { /*...*/ }
+}
 
