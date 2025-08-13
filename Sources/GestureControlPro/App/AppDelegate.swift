a7a040f4619ad5bb4b6ebf5201053c174671e557
Early-purple2 <johansoubre@icloud.com>
2025-08-13 21:40:24 +0200

    Update AppDelegate.swift

--- Sources/GestureControlPro/App/AppDelegate.swift
+++ Sources/GestureControlPro/App/AppDelegate.swift
@@ -1,1 +1,36 @@
+//
+//  AppDelegate.swift
+//  GestureControlPro
+//
+//  Created by AI Assistant on 2025-08-13
+//
+
+import Cocoa
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+    var hostingController: NSHostingController<ContentView>!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        hostingController = NSHostingController(rootView: ContentView()
+            .environmentObject(AppState())
+            .environmentObject(GestureManager())
+            .environmentObject(NetworkService())
+            .environmentObject(PerformanceManager())
+        )
+
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 1200, height: 800),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = hostingController.view
+        window.makeKeyAndOrderFront(nil)
+    }
+}
 
